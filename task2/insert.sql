0f312656ab099d608cc56e732b87affb007fa757
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-03-17 15:09:01 +0300

    Create insert.sql

---
+++ task2/insert.sql
@@ -0,0 +1,200 @@
+insert into genre(name)
+values ('metalcore');
+
+insert into genre(name)
+values ('progressive metalcore');
+
+insert into genre(name)
+values ('deathcore');
+
+insert into genre(name)
+values ('hardcore');
+
+insert into genre(name)
+values ('post hardcore');
+
+insert into genre(name)
+values ('djent');
+
+insert into performers (name)
+values ('Every Hour Kills');
+
+insert into performers (name)
+values ('By The Thousands');
+
+insert into performers (name)
+values ('Erra');
+
+insert into performers (name)
+values ('Vildhjarta');
+
+insert into performers (name)
+values ('Alpha Wolf');
+
+insert into performers (name)
+values ('Reflections');
+
+insert into performers (name)
+values ('Chelsea Grin');
+
+insert into performers (name)
+values ('S');
+
+insert into album (name, year_of_issue)
+values ('The Descent', '2019-01-08');
+
+insert into album (name, year_of_issue)
+values ('Cure', '2020-04-05');
+
+insert into album (name, year_of_issue)
+values ('Willow', '2018-10-13');
+
+insert into album (name, year_of_issue)
+values ('Passing', '2021-11-21');
+
+insert into album (name, year_of_issue)
+values ('Inward', '2020-09-26');
+
+insert into album (name, year_of_issue)
+values ('Heavener', '2019-03-03');
+
+insert into album (name, year_of_issue)
+values ('Eternal blue', '2020-08-22');
+
+insert into genre_performers 
+values (1, 1);
+
+insert into genre_performers 
+values (1, 2);
+
+insert into genre_performers 
+values (2, 1);
+
+insert into genre_performers 
+values (2, 2);
+
+insert into genre_performers 
+values (3, 6);
+
+insert into genre_performers 
+values (3, 7);
+
+insert into genre_performers 
+values (4, 8);
+
+insert into genre_performers 
+values (5, 8);
+
+insert into genre_performers 
+values (6, 4);
+
+insert into performers_album 
+values (1, 1);
+
+insert into performers_album 
+values (2, 2);
+
+insert into performers_album 
+values (3, 3);
+
+insert into performers_album 
+values (4, 4);
+
+insert into performers_album 
+values (5, 5);
+
+insert into performers_album 
+values (6, 6);
+
+insert into performers_album 
+values (7, 7);
+
+insert into performers_album 
+values (7, 8);
+
+insert into track (album_id, "name",duration)
+values (1, 'Comatose', '3:20');
+
+insert into track (album_id, "name",duration)
+values (1, 'Unbound', '4:48');
+
+insert into track (album_id, "name",duration)
+values (2, 'Acheron', '4:32');
+
+insert into track (album_id, "name",duration)
+values (2, 'Cursed', '3:40');
+
+insert into track (album_id, "name",duration)
+values (3, 'The Flood', '2:52');
+
+insert into track (album_id, "name",duration)
+values (3, 'Deadweight', '3:30');
+
+insert into track (album_id, "name",duration)
+values (4, 'Shiver', '4:25');
+
+insert into track (album_id, "name",duration)
+values (4, 'Parasite', '2:53');
+
+insert into track (album_id, "name",duration)
+values (5, 'Tempest', '2:56');
+
+insert into track (album_id, "name",duration)
+values (5, 'Darkest Frame', '3:52');
+
+insert into track (album_id, "name",duration)
+values (6, 'Werifesteria', '4:06');
+
+insert into track (album_id, "name",duration)
+values (6, 'mytrack', '4:08');
+
+insert into track (album_id, "name",duration)
+values (7, 'Nemesis', '5:17');
+
+insert into playlist ("name", year_of_issue)
+values ('playlist1','2017-03-21');
+
+insert into playlist ("name", year_of_issue)
+values ('playlist2','2019-03-21');
+
+insert into playlist ("name", year_of_issue)
+values ('playlist3','2018-05-23');
+
+insert into playlist ("name", year_of_issue)
+values ('playlist4','2020-12-21');
+
+insert into playlist ("name", year_of_issue)
+values ('playlist5','2021-08-24');
+
+insert into playlist_track 
+values (1, 1);
+
+insert into playlist_track 
+values (1, 5);
+
+insert into playlist_track 
+values (1, 6);
+
+insert into playlist_track 
+values (2, 4);
+
+insert into playlist_track 
+values (2, 1);
+
+insert into playlist_track 
+values (2, 7);
+
+insert into playlist_track 
+values (3, 8);
+
+insert into playlist_track 
+values (3, 9);
+
+insert into playlist_track 
+values (4, 10);
+
+insert into playlist_track 
+values (4, 11);
+
+insert into playlist_track 
+values (5, 12);
